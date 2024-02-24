cb0f1307a4777217d737b52da2356dbf2dac949d
David Stuart <davidstuart199@gmail.com>
2024-02-23 22:15:32 -0500

    just adding some stuff not really a binary search at this point

--- binary-search.groovy
+++ binary-search.groovy
@@ -1,5 +1,22 @@
-def nl = [1,2,3,4]
+def nl = [1,2,6,7,12,0,3,4]
+def nlSize = nl.size()
+def nlMidPt = nl.size() / 2
+println(nl.sort())
+println("list contents: " + nl)
+println("list size: " + nlSize)
+println("list approx mid pt: " + nlMidPt)
+println(nlMidPt)
 
-for(int i in nl) {
-    println(i)
+print("Target Number: ")
+userInput = System.console().readLine() as Integer
+
+println("You entered: $userInput")
+
+if(nl[nlMidPt] == userInput) {
+    println("Target found")
+} else {
+    for(int i in nl) {
+        println(i)
+    }
+
 }
